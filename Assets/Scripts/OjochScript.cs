7507a6c13f86d6ff41731efca8b932d640a7b538
TondaFS <Holik.Antonin@gmail.com>
2015-10-07 09:25:37 +0200

    Vytvoření Ojocha
    Hráčem ovládaný Ojoch.

---
+++ Assets/Scripts/OjochScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Ovládání a chování Ojocha = Hráče
+/// </summary>
+
+public class OjochScript : MonoBehaviour {
+
+    /// <summary>
+    /// Proměnné
+    /// </summary>
+
+    private float rychlost = 10f;
+    public Vector2 speed = new Vector2(10,10);   // Rychlost Ojocha
+    private Vector2 movement;                   // Ulozeni pohybu
+
+    void Update () {
+        //Axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // Pohyb 
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+
+    void FixedUpdate() {
+       GetComponent<Rigidbody2D>().velocity = movement; //Aplikace pohybu na objekt
+    }
+
+}
