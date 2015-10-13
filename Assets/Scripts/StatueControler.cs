4d25f664224d1c1c2a6dbcebec768a56d5947d90
TondaFS <Holik.Antonin@gmail.com>
2015-10-13 10:46:37 +0200

    Prototype build
    #socha

---
+++ Assets/Scripts/StatueControler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatueControler : MonoBehaviour {
+
+    float c;
+	// Update is called once per frame
+	void Update () {
+        c += 0.2f;
+        Vector2 sine = new Vector2(transform.position.x, 1 + Mathf.Sin(c));
+        transform.position = sine;
+    }
+}
