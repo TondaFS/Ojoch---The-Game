fa0fa7fa9212d135a7c5d7d46be0e0a2b310adfc
TondaFS <Holik.Antonin@gmail.com>
2015-11-21 17:43:34 +0100

    Aktualizace shotu bubliny

--- Assets/Scripts/MovingScript.cs
+++ Assets/Scripts/MovingScript.cs
@@ -10,23 +10,24 @@ public class MovingScript : MonoBehaviour {
     //Proměnné
     public Vector2 speed = new Vector2(10, 10);     //rychlost
     public Vector2 direction = new Vector2(-1, 0);  //smer
-    public float modifier = 0f;                     //modifikator gravitace
-    public float countdown = 5f;                    //odpocet
+    //public float modifier = 0f;                     //modifikator gravitace
+    //public float countdown = 5f;                    //odpocet
     private Vector2 movement;                       //pohyb
 
     void Update() {
-        countdown -= Time.deltaTime;             
-        movement = new Vector2(speed.x * direction.x, speed.y * direction.y);   //samotny pohyb
-
+        //countdown -= Time.deltaTime;             
+        movement = new Vector2(speed.x * direction.x * Time.deltaTime, speed.y * direction.y * Time.deltaTime);   //samotny pohyb
+        transform.Translate(movement, 0);
+        /*
         //pokud uplyne doba countdown, zacne se na objekt aplikovat gravitace s modifikatorem
         if (countdown <= 0.0f) {
             gameObject.GetComponent<Rigidbody2D>().gravityScale -= (float)modifier * 2;
-        }            
+        }  */          
         
     }
 
     void FixedUpdate() {
-        GetComponent<Rigidbody2D>().velocity = movement; //Aplikace pohybu na objekt
+        //GetComponent<Rigidbody2D>().velocity = movement; //Aplikace pohybu na objekt
     }
 
 
