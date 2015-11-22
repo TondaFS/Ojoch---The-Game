6f295289fa89c663fc26238f3d14ce53b60b66df
nanookilla <janszlauer@gmail.com>
2015-11-23 00:37:42 +0100

    Socha chodí + ptáci jsou zlí

---
+++ Assets/Scripts/enemyBirdController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBirdController : MonoBehaviour {
+
+    public Animator animator;
+    public bool upDown;
+    public bool upDownMore;
+
+	void Awake () {
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("upDown", upDown);
+        animator.SetBool("upDownMore", upDownMore);
+	}
+}
